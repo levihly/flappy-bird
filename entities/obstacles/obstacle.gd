0ff23cf84e04d9978a1e5e8ee34af514b75ec5b4
Levi Ly <levi.h.ly@gmail.com>
2025-02-17 18:39:20 -0700

    Polish behavior and UI

--- entities/obstacles/obstacle.gd
+++ entities/obstacles/obstacle.gd
@@ -1,4 +1,4 @@
-extends Node2D
+class_name Obstacle extends Node2D
 
 @onready var top: RigidBody2D = $Top
 @onready var bottom: RigidBody2D = $Bottom
@@ -9,10 +9,26 @@ signal point_added
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+
 	position.y = randi() % 201 + 80
 	top.linear_velocity.x = -(speed)
 	bottom.linear_velocity.x = -(speed)
 
+func slow() -> void:
+	var top_tween = get_tree().create_tween()
+	var bottom_tween = get_tree().create_tween()
+	top_tween.tween_property(top, "linear_velocity", Vector2.ZERO, 2.5)
+	bottom_tween.tween_property(bottom, "linear_velocity", Vector2.ZERO, 2.5)
+
 
 func _on_point_detection_body_entered(body: Node2D) -> void:
 	point_added.emit()
+
+
+func _on_visible_on_screen_notifier_screen_exited() -> void:
+	print('left screen')
+	queue_free()
+
+
+func _on_visible_on_screen_notifier_screen_entered() -> void:
+	print('entered screen')
--- game/game.gd
+++ game/game.gd
@@ -3,51 +3,42 @@ extends Node2D
 const OBSTACLE = preload("res://entities/obstacles/obstacle.tscn")
 
 @export var obstacle_spawn_interval : float = 1.25
-@export var obstacle_despawn_interval : float = 5.0
 
 @onready var player: CharacterBody2D = $Player
 @onready var ui: CanvasLayer = $UI
 
 var player_is_alive : bool = true
-var time_tween : Tween
-
-signal game_started
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	player.died.connect(_on_player_death)
 	player.started.connect(_on_player_started)
-	if time_tween:
-		time_tween.remove_all()
-		
-	Engine.time_scale = 1.0
-	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	print(get_child_count())
 
 func spawn_obstacle() -> void:
-
+	
+	if not player_is_alive:
+		return
 		
 	var obstacle = OBSTACLE.instantiate()
 	add_child(obstacle)
 	obstacle.point_added.connect(_on_player_got_point)
 	
-	
-	
 	await get_tree().create_timer(obstacle_spawn_interval, false).timeout
 	spawn_obstacle()
 
-	# delete self after time
-	await get_tree().create_timer(obstacle_despawn_interval, false).timeout
-	obstacle.queue_free()
 
 func _on_player_death() -> void:
 	player_is_alive = false
-	time_tween = create_tween()
-	time_tween.tween_property(Engine, "time_scale", 0.0, 0.5)
+	
+	for obstacle in get_children():
+		if obstacle is Obstacle:
+			obstacle.slow()
+	
 	ui.show_game_over()
 
 func _on_player_started() -> void:
